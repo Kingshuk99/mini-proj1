1ed03b2707a891d61ffe61e88d9ffb0555715625
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2024-08-04 21:23:25 +0530

    Update Revenue.js

--- src/Components/Revenue.js
+++ src/Components/Revenue.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react"
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
 const Revenue = () => {
     const [orders, setOrders] = useState([]);
     const [revenue, setRevenue] = useState(0);
     useEffect(() => {
         const fetchOrders = async () => {
-            const response = await fetch(`http://localhost:3030/order`);
+            const response = await fetch(`${backendUrl}/order`);
             const result = await response.json();
             setOrders(result);
             var tot = 0;
