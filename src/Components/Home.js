696b22ae91e11cd142776d59b69ba154bf769fbe
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2024-09-06 00:50:13 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,13 +5,13 @@ const Home = () => {
     <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
     <div className="carousel-inner">
     <div className="carousel-item active" data-bs-interval="10000">
-      <img src="https://img.freepik.com/free-photo/freshly-italian-pizza-with-mozzarella-cheese-slice-generative-ai_188544-12347.jpg?size=626&ext=jpg&ga=GA1.1.1887574231.1711670400&semt=ais" className="d-block w-100" alt="..." width={500} height={550}/>
+      <img src="https://img.freepik.com/free-photo/freshly-italian-pizza-with-mozzarella-cheese-slice-generative-ai_188544-12347.jpg?size=626&ext=jpg&ga=GA1.1.1887574231.1711670400&semt=ais"  className="vh-100 vw-100" alt="..."/>
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-      <img src="https://hips.hearstapps.com/hmg-prod/images/is-pizza-healthy-1671729626.jpg" className="d-block w-100" alt="..." width={400} height={550}/>
+      <img src="https://hips.hearstapps.com/hmg-prod/images/is-pizza-healthy-1671729626.jpg"  className="vh-100 vw-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg" className="d-block w-100" alt="..." width={550} height={550}/>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg"  className="vh-100 vw-100" alt="..."/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
